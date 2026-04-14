cfdc7a9427fb728d23d10459632eb5f80da8bcb9
aberkanesofianeoeb-eng <aberkanesofiane.oeb@gmail.com>
2026-04-15 00:19:46 +0100

    Update participants.js

--- api/participants.js
+++ api/participants.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const FIREBASE_URL = 'https://qrcode-54ca8-default-rtdb.firebaseio.com';
+  const FIREBASE_URL = 'https://console.firebase.google.com/project/qrcode-54ca8/database/qrcode-54ca8-default-rtdb/data/~2F';
 
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
